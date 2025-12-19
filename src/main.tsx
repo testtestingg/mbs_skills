f5300c3e6d1a683b4b8426bb605a26b538304f42
testtestingg <dridixhydra@gmail.com>
2025-12-19 18:41:08 +0100

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,10 @@
-// src/main.tsx
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { HelmetProvider } from 'react-helmet-async';
-import App from './App';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
 import './index.css';
 
-// Prevent service worker registration
-if ('serviceWorker' in navigator) {
-  // Remove any existing service workers
-  navigator.serviceWorker.getRegistrations().then(registrations => {
-    registrations.forEach(registration => {
-      registration.unregister();
-    });
-  });
-}
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-
-root.render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
 );
